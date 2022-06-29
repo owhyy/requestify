0798d3fa183ab594e599ab1c703f8a6021d0d816
Babin Ion <ionbabin123@mail.ru>
2022-06-29 15:17:56 +0300

    added list display

--- requestify.py
+++ requestify.py
@@ -6,8 +6,12 @@ import sys
 import argparse
 import pyperclip
 import json
+from collections import defaultdict
 from urllib import parse, request
 from black import format_str, FileMode
+
+# name that will be used for class with requests
+REQUESTS_CLASS_NAME = "RequestsTest"
 
 
 def get_data_dict(query):
@@ -15,7 +19,7 @@ def get_data_dict(query):
     return _ if len(_) > 0 else query
 
 
-class __Generate(object):
+class RequestifyObject(object):
     def __init__(self, base_string):
         self.base_string = base_string.strip()
         self.url = ""
@@ -40,12 +44,6 @@ class __Generate(object):
         self.__write_to_file(
             filename, with_headers=with_headers, with_cookies=with_cookies
         )
-
-    # def to_current_file(self, with_headers=True, with_cookies=True):
-    #     current_filename = sys.argv[0].split("/")[-1]
-    #     self.__write_to_file(
-    #         current_filename, with_headers=with_headers, with_cookies=with_cookies
-    #     )
 
     def to_screen(self, with_headers=True, with_cookies=True):
         self.__write_to_stdio(with_headers, with_cookies)
@@ -100,54 +98,140 @@ class __Generate(object):
                 # self.__update_length(k)
         return self.headers
 
-    def __create_response(self, with_headers=True, with_cookies=True):
+    # returns base(without imports, only the text), unbeautified string
+    def create_responses_base(self, indent="", with_headers=True, with_cookies=True):
         request_options = ""
-        wait_to_write = ["import requests", "\n"]
-
+        wait_to_write = []
         if with_headers:
-            wait_to_write.append(f"headers = {self.headers}\n")
+            wait_to_write.append(f"{indent}headers = {self.headers}")
             request_options += ", headers=headers"
 
         if with_cookies:
-            wait_to_write.append(f"cookies = {self.cookies}\n")
+            wait_to_write.append(f"{indent}cookies = {self.cookies}")
             request_options += ", cookies=cookies"
 
         if self.method == "post":
-            # if type(self.data) == dict:
-            #     cute_data = self.__beautify(self.data, space=8)
-            # else:
-            wait_to_write.append(f"data = {self.data}\n")
+            wait_to_write.append(f"{indent}data = {self.data}")
             request_options += ", data=data"
 
         wait_to_write.append(
-            f"response = requests.{self.method}('{self.url}'" + request_options + ")"
+            f"{indent}response = requests.{self.method}('{self.url}'"
+            + request_options
+            + ")"
         )
-        wait_to_write.append("\nprint(response.text)")
-        return self.__beautify_response("".join(wait_to_write))
-        # return self.__beautify_response(wait_to_write)
+
+        wait_to_write.append(f"{indent}print(response.text)")
+        return f"\n".join(wait_to_write)
+
+    # returns beautified string
+    def __create_responses_string(self, with_headers=True, with_cookies=True):
+        request_options = ""
+        response = self.create_responses_base("", with_headers, with_cookies)
+        wait_to_write = [
+            "import requests",
+            "\n",
+            response,
+            "\n\n",
+        ]
+
+        return self.__beautify_string("".join(wait_to_write))
 
     @staticmethod
-    def __beautify_response(response):
-        s = "".join(x for x in response)
-        return format_str(s, mode=FileMode())
+    def __beautify_string(response):
+        return format_str(response, mode=FileMode())
 
     def __write_to_file(self, file, with_headers=True, with_cookies=True):
-        request = self.__create_response(with_headers, with_cookies)
+        request = self.__create_responses_string(with_headers, with_cookies)
         with open(file, "w") as f:
             f.write("\n".join(request) + "\n")
 
     def __write_to_stdio(self, with_headers=True, with_cookies=True):
-        request = self.__create_response(with_headers, with_cookies)
+        request = self.__create_responses_string(with_headers, with_cookies)
         print(request)
+
+
+class __GenerateList(object):
+    def __init__(self, base_list):
+        self.base_list = base_list
+        self.requests = []
+        self.existing_function_names = defaultdict(int)
+        self.__generate()
+
+    def __generate(self):
+        for curl in self.base_list:
+            request = RequestifyObject(curl)
+            self.requests.append(request)
+
+    def __create_responses_text(self):
+        requests_text = [
+            "import requests",
+            "\n\n",
+            f"class {REQUESTS_CLASS_NAME}():",
+            "\n",
+        ]
+        function_names = []
+        for request in self.requests:
+            function_name = self.__create_function_name(request)
+            function_names.append(function_name)
+
+            response = request.create_responses_base(indent="\t\t")
+            requests_text.append(f"\tdef {function_name}(self):{response}")
+            requests_text.append("\n")
+
+        requests_text.append("\n\t")
+        requests_text.append("def call_all(self):")
+        requests_text.append("\n\t\t")
+        requests_text.append(
+            "\n\t\t".join(
+                [f"self.{function_name}()" for function_name in function_names]
+            )
+        )
+
+        requests_text.append("\n\n")
+        requests_text.append("if __name__ == '__main__': ")
+        requests_text.append("\n\t")
+        requests_text.append(f"{REQUESTS_CLASS_NAME}().call_all()")
+        # return "".join(requests_text)
+        return format_str("".join(requests_text), mode=FileMode())
+
+    # TODO: test this
+    def __create_function_name(self, request):
+        _ = request.url.split("/")
+        url = _[2] if len(_) > 2 else _[0]
+        url = url.replace(".", "_")
+
+        function_name = f"{url}_{request.method}"
+
+        function_count = self.existing_function_names[function_name]
+
+        ret = f"{function_name}{'_' + str(function_count) if function_count else ''}"
+
+        self.existing_function_names[function_name] += 1
+        return ret
+
+    def to_file(self, filename):
+        self.__write_to_file(filename)
+
+    def to_screen(self):
+        self.__write_to_stdio()
+
+    def __write_to_file(self, file):
+        requests_as_functions = self.__create_responses_text()
+
+        with open(file, "w") as f:
+            f.write("\n".join(requests_as_functions) + "\n")
+
+    def __write_to_stdio(self, with_headers=True, with_cookies=True):
+        requests_as_functions = self.__create_responses_text()
+        print(requests_as_functions)
 
 
 def __get_file(filename):
     requests = []
-    curl = ""
     with open(filename, mode="r") as in_file:
         for line in in_file:
-            curl += line
-        return curl
+            requests.append(line)
+    return requests
 
 
 def __get_clipboard():
@@ -155,16 +239,21 @@ def __get_clipboard():
 
 
 def from_string(base_string):
-    return __Generate(base_string)
+    return RequestifyObject(base_string)
 
 
 def from_clipboard():
-    print(__get_clipboard())
-    return __Generate(__get_clipboard())
+    return RequestifyObject(__get_clipboard())
 
 
 def from_file(filename):
-    return __Generate(__get_file(filename))
+    requests_from_file = __get_file(filename)
+    assert requests_from_file, "No data in the specified file"
+    if len(requests_from_file) == 1:
+        requests = RequestifyObject(requests_from_file[0])
+    else:
+        requests = __GenerateList(requests_from_file)
+    return requests
 
 
 flags_and_messages = {
@@ -221,3 +310,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # print(len(__get_file("curls")))
