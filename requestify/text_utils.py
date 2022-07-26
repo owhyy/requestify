466e2568c1e16bc1b5f1cca83a1ae7a0c5710afa
Babin Ion <ionbabin123@mail.ru>
2022-07-26 11:26:24 +0300

    added text generation for RequstifyObject

--- requestify/text_utils.py
+++ requestify/text_utils.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
+from typing import Any
+
 REQUEST_VARIABLE_NAME = "request"
+REQUEST_CLASS_NAME = "RequestsTest"
 
 FunctionTextType = tuple[str, list[str]]
+ClassTextType = tuple[str, list[FunctionTextType]]
 
 """
 General
@@ -13,12 +17,130 @@ def generate_imports_text(*packages: str) -> list[str]:
 
 
 """
+Function text
+"""
+
+
+def __generate_function_text(
+    function_name: str, *function_body: str, is_in_class: bool
+) -> FunctionTextType:
+    name_part = f"""def {function_name}{"(self)" if is_in_class else "()"}:"""
+    function_part = [*function_body]
+    function_text = (name_part, function_part)
+    return function_text
+
+
+def __generate_indented_function(
+    function_name: str, *function_body: str, is_in_class: bool
+) -> FunctionTextType:
+    function_text = __generate_function_text(
+        function_name, *function_body, is_in_class=is_in_class
+    )
+    indented_function = (
+        indent_function_inside_class(function_text)
+        if is_in_class
+        else indent_function_outside_class(function_text)
+    )
+    return indented_function
+
+
+def generate_function_text_inside_class(
+    function_name: str, *function_body: str
+) -> FunctionTextType:
+    return __generate_indented_function(function_name, *function_body, is_in_class=True)
+
+
+def generate_function_text_outside_class(
+    function_name: str, *function_body: str
+) -> FunctionTextType:
+    return __generate_indented_function(
+        function_name, *function_body, is_in_class=False
+    )
+
+
+def __indent_function(function: FunctionTextType, indent_amount=1) -> FunctionTextType:
+    """
+    Indent levels means indent level for body, not for first line.
+    Therefore, a indent level of 1 looks like
+    def fun()
+        pass
+    ^^^^
+    """
+    first_line_indent_amount = indent_amount - 1 if indent_amount > 0 else indent_amount
+    first_line_indent = "\t" * first_line_indent_amount
+    body_indent = "\t" * indent_amount
+
+    function_name = function[0]
+    indented_function_name = first_line_indent + function_name
+
+    indented_function_body = []
+    function_body = function[1]
+    for line in function_body:
+        indented_function_body.append(body_indent + line)
+
+    indented_function = (indented_function_name, indented_function_body)
+    return indented_function
+
+
+def indent_function_inside_class(function: FunctionTextType) -> FunctionTextType:
+    return __indent_function(function, indent_amount=2)
+
+
+def indent_function_outside_class(
+    function: FunctionTextType,
+) -> FunctionTextType:
+    return __indent_function(function)
+
+
+"""
+Class text
+"""
+
+
+def _generate_class_body(*functions: tuple[str, Any]) -> list[FunctionTextType]:
+    class_body = []
+    for function in functions:
+        class_body.append(generate_function_text_inside_class(*function))
+    return class_body
+
+
+# generate class for tuple
+# of function name and function body
+def generate_class_text_from_ungenerated_functions(
+    class_name: str, *class_functions: tuple[str, Any]
+) -> ClassTextType:
+    class_body = _generate_class_body(*class_functions)
+    # indented_class_body = indent_class_body(*class_body)
+    class_text = (f"class {class_name}():", class_body)
+    return class_text
+
+
+# generates class for functions that were already
+# created with generate_function_text_* functions
+def generate_class_text_from_generated_functions(
+    class_name: str, *class_body: FunctionTextType
+) -> ClassTextType:
+    indented_class_body = indent_class_body(*class_body)
+    class_text = (f"class {class_name}():", indented_class_body)
+    return class_text
+
+
+def indent_class_body(
+    *class_body: FunctionTextType,
+) -> list[FunctionTextType]:
+    indented_class_body = [
+        indent_function_inside_class(function) for function in class_body
+    ]
+    return indented_class_body
+
+
+"""
 Requestify text
 """
 
 
 def generate_requestify_base_text(
-    req: RequestifyObject, with_headers=True, with_cookies=True
+    req: _RequestifyObject, with_headers=True, with_cookies=True
 ) -> list[str]:
     requestify_text = []
     request_options = ""
@@ -43,126 +165,36 @@ def generate_requestify_base_text(
 
 
 def generate_requestify_function(
-    req: RequestifyObject, with_headers=True, with_cookies=True
-) -> FunctionTextType | None:
+    req: _RequestifyObject, with_headers=True, with_cookies=True
+) -> FunctionTextType:
     request_text = generate_requestify_base_text(req, with_headers, with_cookies)
     return generate_function_text_outside_class(req._function_name, *request_text)
 
 
-"""
-Function text
-"""
-
-
-def __generate_function_text(
-    function_name: str, *function_body: str, is_in_class: bool
-) -> FunctionTextType | None:
-    function_text = None
-
-    if function_body:
-        function_part = [*function_body]
-
-        name_part = f"""def {function_name}{"(self)" if is_in_class else "()"}:"""
-        function_text = (name_part, function_part)
-
-    return function_text
-
-
-def __generate_indented_function(
-    function_name: str, *function_body: str, is_in_class: bool
-) -> FunctionTextType | None:
-    function_text = __generate_function_text(
-        function_name, *function_body, is_in_class=is_in_class
-    )
-
-    if function_text:
-        indented_function = (
-            indent_function_inside_class(function_text)
-            if is_in_class
-            else indent_function_outside_class(function_text)
-        )
-
-    else:
-        indented_function = None
-    return indented_function
-
-
-def generate_function_text_inside_class(
-    function_name: str, *function_body: str
-) -> FunctionTextType | None:
-    return __generate_indented_function(function_name, *function_body, is_in_class=True)
-
-
-def generate_function_text_outside_class(
-    function_name: str, *function_body: str
-) -> FunctionTextType | None:
-    return __generate_indented_function(
-        function_name, *function_body, is_in_class=False
+def generate_requestify_class(
+    req: _RequestifyObject, with_headers=True, with_cookies=True
+) -> ClassTextType:
+    request_text = generate_requestify_base_text(req, with_headers, with_cookies)
+    return generate_class_text_from_ungenerated_functions(
+        REQUEST_CLASS_NAME, (req._function_name, *request_text)
     )
 
 
-def __indent_function(
-    function: FunctionTextType, indent_amount=1
-) -> FunctionTextType | None:
-    """
-    Indent levels means indent level for body, not for first line.
-    Therefore, a indent level of 1 looks like
-    def fun()
-        pass
-    ^^^^
-    """
-    if function:
-        first_line_indent_amount = (
-            indent_amount - 1 if indent_amount > 0 else indent_amount
-        )
-        first_line_indent = "\t" * first_line_indent_amount
-        body_indent = "\t" * indent_amount
-
-        function_name = function[0]
-        indented_function_name = first_line_indent + function_name
-
-        indented_function_body = []
-        function_body = function[1]
-        for line in function_body:
-            indented_function_body.append(body_indent + line)
-
-        indented_function = (indented_function_name, indented_function_body)
-    else:
-        indented_function = None
-
-    return indented_function
+def generate_requestify_list_function(loreq: _RequestifyList) -> FunctionTextType:
+    pass
 
 
-def indent_function_inside_class(
-    function: FunctionTextType
-) -> FunctionTextType | None:
-    return __indent_function(function, indent_amount=2)
+def generate_requestify_list_class(loreq: _RequestifyList) -> ClassTextType:
+    pass
 
 
-def indent_function_outside_class(
-    function: FunctionTextType
-) -> FunctionTextType | None:
-    return __indent_function(function)
+def generate_replacement_base_text(rreq: _ReplaceRequestify) -> list[str]:
+    pass
 
 
-"""
-Class text
-"""
+def generate_replacement_function(rreq: _ReplaceRequestify) -> FunctionTextType:
+    pass
 
 
-def generate_class_text(
-        class_name: str, *class_body: FunctionTextType | None
-) -> tuple[str, list[FunctionTextType | None]]:
-    indented_class_body = indent_class_body(class_body) # type: list[FunctionTextType | None]
-    class_text = (f"class {class_name}():", indented_class_body)
-
-    return class_text
-
-
-def indent_class_body(
-    class_body: list[FunctionTextType]
-) -> list[FunctionTextType | None]:
-    indented_class_body = [
-        indent_function_inside_class(function) for function in class_body
-    ]
-    return indented_class_body
+def generate_replacement_class(rreq: _ReplaceRequestify) -> ClassTextType:
+    pass
--- tests/test_text_utils.py
+++ tests/test_text_utils.py
@@ -2,7 +2,6 @@ import pytest
 from requestify.models import _RequestifyObject
 from requestify import text_utils
 
-REQUEST_VARIABLE_NAME = "request"
 GOOGLE = "https://google.com"
 
 
@@ -11,7 +10,7 @@ def unindented_function():
     return ("def function_name():", ['print("i am a function body")'])
 
 
-class TestTextUtils:
+class TestBaseGeneration:
     def test_generate_imports(self):
         assert text_utils.generate_imports_text("unittest", "numpy", "json") == [
             "import unittest",
@@ -21,42 +20,6 @@ class TestTextUtils:
 
     def test_generate_imports_no_args(self):
         assert len(text_utils.generate_imports_text()) == 0
-
-    def test_base_response_with_headers_with_cookies(self):
-        req = _RequestifyObject(f"""curl -X post '{GOOGLE}' -H 'x: y'""")
-        base_response_text = [
-            "headers = {'x': 'y'}",
-            "cookies = {}",
-            f"{REQUEST_VARIABLE_NAME} = requests.{req._method}('{req._url}', headers=headers, cookies=cookies)",
-        ]
-        assert (
-            text_utils.generate_requestify_base_text(req, True, True)
-            == base_response_text
-        )
-
-    def test_base_response_no_headers_no_cookies(self):
-        req = _RequestifyObject(f"curl -X GET '{GOOGLE}'")
-        base_response_text = [
-            f"{REQUEST_VARIABLE_NAME} = requests.get('{GOOGLE}')",
-        ]
-        assert (
-            text_utils.generate_requestify_base_text(
-                req, with_headers=False, with_cookies=False
-            )
-            == base_response_text
-        )
-
-    def test_generate_requestify_function(self):
-        req = _RequestifyObject(f"curl -X GET '{GOOGLE}'")
-        text = (
-            f"def {req._function_name}():",
-            [
-                "\theaders = {}",
-                "\tcookies = {}",
-                f"\t{REQUEST_VARIABLE_NAME} = requests.get('{GOOGLE}', headers=headers, cookies=cookies)",
-            ],
-        )
-        assert text_utils.generate_requestify_function(req) == text
 
     def test_generate_function_text_inside_class(self):
         function_in_class = (
@@ -96,21 +59,6 @@ class TestTextUtils:
             == function_outside_class
         )
 
-    def test_generate_function_text_no_body(self):
-        assert (
-            text_utils.generate_function_text_inside_class(
-                function_name="function_without_body"
-            )
-            is None
-        )
-
-        assert (
-            text_utils.generate_function_text_outside_class(
-                function_name="function_without_body"
-            )
-            is None
-        )
-
     def test_indent_function_inside_class(self, unindented_function):
         indented_function = (
             "\tdef function_name():",
@@ -135,44 +83,169 @@ class TestTextUtils:
             == indented_function
         )
 
-    def test_indent_empty_function(self):
-        assert (text_utils.indent_function_inside_class([])) is None
-        assert (text_utils.indent_function_outside_class([])) is None
-
     def test_indent_class_body(self):
         unindented_class_function = (
-            "define function_name(self):",
+            "def function_name(self):",
             [
                 'print("i am a function body")',
             ],
         )
 
         indented_class_function = (
-            "\tdefine function_name(self):",
+            "\tdef function_name(self):",
             [
                 '\t\tprint("i am a function body")',
             ],
         )
-        unindented_class_body = [unindented_class_function] * 3
-        indented_class_body = [indented_class_function] * 3
+        # unindented_class_body = [unindented_class_function] * 3
+        # indented_class_body = [indented_class_function] * 3
 
-        assert (
-            text_utils.indent_class_body(unindented_class_body) == indented_class_body
-        )
+        assert text_utils.indent_class_body(unindented_class_function) == [
+            indented_class_function
+        ]
 
-    def test_generate_class_text(self):
+    def test_generate_class_from_generated_functions(self):
         unindented_class_function = (
-            "define function_name(self):",
+            "def function_name(self):",
             [
                 'print("i am a function body")',
             ],
         )
         indented_class_function = (
-            "\tdefine function_name(self):",
+            "\tdef function_name(self):",
             [
                 '\t\tprint("i am a function body")',
             ],
         )
-        assert text_utils.generate_class_text(
+        assert text_utils.generate_class_text_from_generated_functions(
             "SomeClass", unindented_class_function, unindented_class_function
         ) == ("class SomeClass():", [indented_class_function, indented_class_function])
+
+    def test_generate_class_functions_from_ungenerated_functions(self):
+        function = ("function_name", "foo = bar", "bar = foo")
+        assert text_utils.generate_class_text_from_ungenerated_functions(
+            "baz", function
+        ) == (
+            "class baz():",
+            [("\tdef function_name(self):", ["\t\tfoo = bar", "\t\tbar = foo"])],
+        )
+
+
+class TestModelTextGeneration(object):
+    def test_base_requstify_with_headers_with_cookies(self):
+        req = _RequestifyObject(f"""curl -X post '{GOOGLE}' -H 'x: y'""")
+        base_response_text = [
+            "headers = {'x': 'y'}",
+            "cookies = {}",
+            f"{text_utils.REQUEST_VARIABLE_NAME} = requests.{req._method}('{req._url}', headers=headers, cookies=cookies)",
+        ]
+        assert (
+            text_utils.generate_requestify_base_text(req, True, True)
+            == base_response_text
+        )
+
+    def test_base_requestify_no_headers_no_cookies(self):
+        req = _RequestifyObject(f"curl -X GET '{GOOGLE}'")
+        base_response_text = [
+            f"{text_utils.REQUEST_VARIABLE_NAME} = requests.get('{GOOGLE}')",
+        ]
+        assert (
+            text_utils.generate_requestify_base_text(
+                req, with_headers=False, with_cookies=False
+            )
+            == base_response_text
+        )
+
+    def test_generate_requestify_function(self):
+        req = _RequestifyObject(f"curl -X GET '{GOOGLE}'")
+        text = (
+            f"def {req._function_name}():",
+            [
+                "\theaders = {}",
+                "\tcookies = {}",
+                f"\t{text_utils.REQUEST_VARIABLE_NAME} = requests.get('{GOOGLE}', headers=headers, cookies=cookies)",
+            ],
+        )
+        assert text_utils.generate_requestify_function(req) == text
+
+    def test_generate_requestify_class(self):
+        req = _RequestifyObject(f"curl -X GET '{GOOGLE}")
+        text = (
+            f"class {text_utils.REQUEST_CLASS_NAME}():",
+            [
+                (
+                    f"\tdef {req._function_name}(self):",
+                    [
+                        "\t\theaders = {}",
+                        "\t\tcookies = {}",
+                        f"\t\t{text_utils.REQUEST_VARIABLE_NAME} = requests.get('{GOOGLE}', headers=headers, cookies=cookies)",
+                    ],
+                )
+            ],
+        )
+        assert text_utils.generate_requestify_class(req) == text
+
+    # feature not needed yet
+    def test_generate_base_requestify_list(self):
+        pass
+
+    def test_generate_requestify_list_function(self):
+        req = _RequestifyObject(f"curl -X GET '{GOOGLE}")
+        body = (
+            [
+                "\theaders = {}",
+                "\tcookies = {}",
+                f"\t{text_utils.REQUEST_VARIABLE_NAME} = requests.get('{GOOGLE}', headers=headers, cookies=cookies)",
+            ],
+        )
+        reqs = 3 * [req]
+
+        text = [
+            (f"def {reqs[0]}get_google():", body),
+            (f"def {reqs[1]}get_google_1():", body),
+            (f"def {reqs[1]}get_google_2():", body),
+        ]
+
+    def test_generate_requestify_list_class(self):
+        req = _RequestifyObject(f"curl -X GET '{GOOGLE}")
+        body = (
+            [
+                "\theaders = {}",
+                "\tcookies = {}",
+                f"\t{text_utils.REQUEST_VARIABLE_NAME} = requests.get('{GOOGLE}', headers=headers, cookies=cookies)",
+            ],
+        )
+        reqs = 3 * [req]
+
+        text = (
+            f"class {text_utils.REQUEST_CLASS_NAME}:",
+            [
+                (f"\tdef {reqs[0]._function_name}(self):"),
+                [
+                    "\t\theaders = {}",
+                    "\t\tcookies = {}",
+                    f"\t\t{text_utils.REQUEST_VARIABLE_NAME} = requests.get('{GOOGLE}', headers=headers, cookies=cookies)",
+                ],
+                (f"\tdef {reqs[1]._function_name}(self):"),
+                [
+                    "\t\theaders = {}",
+                    "\t\tcookies = {}",
+                    f"\t\t{text_utils.REQUEST_VARIABLE_NAME} = requests.get('{GOOGLE}', headers=headers, cookies=cookies)",
+                ],
+                (f"\tdef {reqs[2]._function_name}(self):"),
+                [
+                    "\t\theaders = {}",
+                    "\t\tcookies = {}",
+                    f"\t\t{text_utils.REQUEST_VARIABLE_NAME} = requests.get('{GOOGLE}', headers=headers, cookies=cookies)",
+                ],
+            ],
+        )
+
+    def test_generate_base_replacement(self):
+        pass
+
+    def test_generate_replacement_function(self):
+        pass
+
+    def test_generate_replacement_class(self):
+        pass
